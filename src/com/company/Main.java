7d4829e6a5e4b27a15561e9f897237a33f360808
DenisMathesE <73132421+DenisMathesE@users.noreply.github.com>
2020-10-19 22:51:38 +0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        char test = '7';
+        char test = '9';
 
         if ( test < '6' ) {
             System.out.println("меньше 6");
