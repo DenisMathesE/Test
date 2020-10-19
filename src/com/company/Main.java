be7f8174ad89b676fbd7be0a30ca6ee782bc6ff7
Денис Свет <denis@Deniss-MacBook-Pro.local>
2020-10-19 22:40:42 +0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char test = '7';
+
+        if ( test < '6' ) {
+            System.out.println("меньше 6");
+        } else if ( test > '6' ) {
+            System.out.println("больше 6");
+        } else if ( test == '6' ) {
+            System.out.println("рвано 6");
+        }
+
+//        ArrayList <String> deliveryCities = new ArrayList<>();
+//
+//        deliveryCities.add("Chicago");
+//        deliveryCities.add("New York");
+//        deliveryCities.add("Toronto");
+//        deliveryCities.add(1, "Berlin");
+//
+//        System.out.println(Arrays.toString(deliveryCities.toArray(new String[0])));
+//
+//
+//        byte b;
+//        int i = 257;
+//        double d = 323.142;
+//        System.out.println("\nПpeoбpaзoвaниe типа int в тип byte.") ;
+//        b = (byte) i;
+//        System.out.println("i и Ь " + i + " " + b);
+//
+//        System.out.println ( " \nПpeoбpaзoвaниe типа double i= (int)d");
+//        i= (int)d;
+//        System.out.println("d и i " + d + " " + i);
+//        System.out.println ( " \nПpeoбpaзoвaниe типа double b = (byte) d");
+//        b = (byte) d;
+//        System.out.println("d и Ь " + d + " " + b);
+
+
+    }
+}
