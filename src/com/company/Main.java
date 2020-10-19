e4d9c69039bf1fdeac76b132b7d878ba7288c16a
DenisMathesE <denis.mathes@yandex.ru>
2020-10-19 22:54:01 +0500

    test2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        char test = '9';
+        char test = '5';
 
         if ( test < '6' ) {
             System.out.println("меньше 6");
